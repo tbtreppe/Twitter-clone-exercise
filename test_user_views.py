dd5f954b3fa1d966ed51535a6b8d0f04cfccf91f
Tracey <ttrepper@yahoo.com>
2022-04-02 14:58:56 -0400

    try to fix test_user_views.py

--- test_user_views.py
+++ test_user_views.py
@@ -8,7 +8,8 @@
 import os
 from unittest import TestCase
 
-from models import db, connect_db, Message, User
+from models import db, connect_db, Message, User, Likes, Follows
+from bs4 import BeautifulSoup
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -33,8 +34,8 @@ db.create_all()
 app.config['WTF_CSRF_ENABLED'] = False
 
 
-class UserViewTestCase(TestCase):
-    """Test views for users."""
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
@@ -48,8 +49,8 @@ class UserViewTestCase(TestCase):
                                     email="test@test.com",
                                     password="testuser",
                                     image_url=None)
-        self.testuser_id = 1122
-        self.testuser_id = self.testuser_id
+        self.testuser_id = 8989
+        self.testuser.id = self.testuser_id
 
         self.u1 = User.signup("abc", "test1@test.com", "password", None)
         self.u1_id = 778
@@ -61,53 +62,85 @@ class UserViewTestCase(TestCase):
         self.u4 = User.signup("testing", "test4@test.com", "password", None)
 
         db.session.commit()
-    
+
     def tearDown(self):
-        """Clean up fouled transactions."""
-
+        resp = super().tearDown()
         db.session.rollback()
+        return resp
 
-    def test_user_index(self):
+    def test_users_index(self):
         with self.client as c:
-            resp= c.get("/users")
-            self.assertIn("testuser", str(resp.data))
-            self.assertIn("abc", str(resp.data))
-            self.assertIn("efg", str(resp.data))
-            self.assertIn("hij", str(resp.data))
+            resp = c.get("/users")
 
-    def test_user_search(self):
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@abc", str(resp.data))
+            self.assertIn("@efg", str(resp.data))
+            self.assertIn("@hij", str(resp.data))
+            self.assertIn("@testing", str(resp.data))
+
+    def test_users_search(self):
         with self.client as c:
             resp = c.get("/users?q=test")
 
-            self.assertIn("testuser", str(resp.data))
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@testing", str(resp.data))            
+
+            self.assertNotIn("@abc", str(resp.data))
+            self.assertNotIn("@efg", str(resp.data))
+            self.assertNotIn("@hij", str(resp.data))
 
     def test_user_show(self):
         with self.client as c:
-            resp=c.get(f"/users/{self.testuser_id}")
-            
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("testuser", str(resp.data))
+            resp = c.get(f"/users/{self.testuser_id}")
 
-    def test_setup_likes(self):
-        msg1 = Message(text="this is a message", user_id=self.testuser.id)
-        msg2 = Message(text="this is another message", user_id=self.testuser.id)
-        msg3 = Message(id=7777, text="different message", user_id=self.u1)
-        
-        db.session.add_all([msg1, msg2, msg3])
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("@testuser", str(resp.data))
+
+    def setup_likes(self):
+        m1 = Message(text="trending warble", user_id=self.testuser_id)
+        m2 = Message(text="Eating some lunch", user_id=self.testuser_id)
+        m3 = Message(id=9876, text="likable warble", user_id=self.u1_id)
+        db.session.add_all([m1, m2, m3])
         db.session.commit()
 
-        l1 = Likes(user_id=self.testuser.id, message_id=7777)
+        l1 = Likes(user_id=self.testuser_id, message_id=9876)
 
         db.session.add(l1)
         db.session.commit()
 
-    def test_add_likes(self):
-        m = Message(id=1984, text="message!!!", user_id=self.u1_id)
+    def test_user_show_with_likes(self):
+        self.setup_likes()
+
+        with self.client as c:
+            resp = c.get(f"/users/{self.testuser_id}")
+
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("@testuser", str(resp.data))
+            soup = BeautifulSoup(str(resp.data), 'html.parser')
+            found = soup.find_all("li", {"class": "stat"})
+            self.assertEqual(len(found), 4)
+
+            # test for a count of 2 messages
+            self.assertIn("2", found[0].text)
+
+            # Test for a count of 0 followers
+            self.assertIn("0", found[1].text)
+
+            # Test for a count of 0 following
+            self.assertIn("0", found[2].text)
+
+            # Test for a count of 1 like
+            self.assertIn("1", found[3].text)
+
+    def test_add_like(self):
+        m = Message(id=1984, text="The earth is round", user_id=self.u1_id)
         db.session.add(m)
         db.session.commit()
-    
+
         with self.client as c:
-            with c.session_transaction() as sess:           
+            with c.session_transaction() as sess:
                 sess[CURR_USER_KEY] = self.testuser_id
 
             resp = c.post("/messages/1984/like", follow_redirects=True)
@@ -115,11 +148,12 @@ class UserViewTestCase(TestCase):
 
             likes = Likes.query.filter(Likes.message_id==1984).all()
             self.assertEqual(len(likes), 1)
+            self.assertEqual(likes[0].user_id, self.testuser_id)
 
-    def test_delete_like(self):
+    def test_remove_like(self):
         self.setup_likes()
 
-        m = Message.query.filter(Message.text=="different message").one()
+        m = Message.query.filter(Message.text=="likable warble").one()
         self.assertIsNotNone(m)
         self.assertNotEqual(m.user_id, self.testuser_id)
 
@@ -127,7 +161,7 @@ class UserViewTestCase(TestCase):
             Likes.user_id==self.testuser_id and Likes.message_id==m.id
         ).one()
 
-       
+        # Now we are sure that testuser likes the message "likable warble"
         self.assertIsNotNone(l)
 
         with self.client as c:
@@ -138,8 +172,25 @@ class UserViewTestCase(TestCase):
             self.assertEqual(resp.status_code, 200)
 
             likes = Likes.query.filter(Likes.message_id==m.id).all()
-    
+            # the like has been deleted
             self.assertEqual(len(likes), 0)
+
+    def test_unauthenticated_like(self):
+        self.setup_likes()
+
+        m = Message.query.filter(Message.text=="likable warble").one()
+        self.assertIsNotNone(m)
+
+        like_count = Likes.query.count()
+
+        with self.client as c:
+            resp = c.post(f"/messages/{m.id}/like", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("Access unauthorized", str(resp.data))
+
+            # The number of likes has not changed since making the request
+            self.assertEqual(like_count, Likes.query.count())
 
     def setup_followers(self):
         f1 = Follows(user_being_followed_id=self.u1_id, user_following_id=self.testuser_id)
@@ -158,7 +209,22 @@ class UserViewTestCase(TestCase):
 
             self.assertEqual(resp.status_code, 200)
 
-            self.assertIn("testuser", str(resp.data))
+            self.assertIn("@testuser", str(resp.data))
+            soup = BeautifulSoup(str(resp.data), 'html.parser')
+            found = soup.find_all("li", {"class": "stat"})
+            self.assertEqual(len(found), 4)
+
+            # test for a count of 0 messages
+            self.assertIn("0", found[0].text)
+
+            # Test for a count of 2 following
+            self.assertIn("2", found[1].text)
+
+            # Test for a count of 1 follower
+            self.assertIn("1", found[2].text)
+
+            # Test for a count of 0 likes
+            self.assertIn("0", found[3].text)
 
     def test_show_following(self):
 
@@ -169,7 +235,10 @@ class UserViewTestCase(TestCase):
 
             resp = c.get(f"/users/{self.testuser_id}/following")
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("abc", str(resp.data))
+            self.assertIn("@abc", str(resp.data))
+            self.assertIn("@efg", str(resp.data))
+            self.assertNotIn("@hij", str(resp.data))
+            self.assertNotIn("@testing", str(resp.data))
 
     def test_show_followers(self):
 
@@ -180,7 +249,10 @@ class UserViewTestCase(TestCase):
 
             resp = c.get(f"/users/{self.testuser_id}/followers")
 
-            self.assertIn("abc", str(resp.data))
+            self.assertIn("@abc", str(resp.data))
+            self.assertNotIn("@efg", str(resp.data))
+            self.assertNotIn("@hij", str(resp.data))
+            self.assertNotIn("@testing", str(resp.data))
 
     def test_unauthorized_following_page_access(self):
         self.setup_followers()
@@ -188,7 +260,7 @@ class UserViewTestCase(TestCase):
 
             resp = c.get(f"/users/{self.testuser_id}/following", follow_redirects=True)
             self.assertEqual(resp.status_code, 200)
-            self.assertNotIn("abc", str(resp.data))
+            self.assertNotIn("@abc", str(resp.data))
             self.assertIn("Access unauthorized", str(resp.data))
 
     def test_unauthorized_followers_page_access(self):
@@ -197,6 +269,5 @@ class UserViewTestCase(TestCase):
 
             resp = c.get(f"/users/{self.testuser_id}/followers", follow_redirects=True)
             self.assertEqual(resp.status_code, 200)
-            self.assertNotIn("abc", str(resp.data))
+            self.assertNotIn("@abc", str(resp.data))
             self.assertIn("Access unauthorized", str(resp.data))
-
